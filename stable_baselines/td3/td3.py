a478dc1258a57246dd2a09373c2f8347b9ad8614
Anssi 'Miffyli' Kanervisto <kaneran21@hotmail.com>
2020-01-03 17:46:43 +0200

    Remove unused import

--- stable_baselines/td3/td3.py
+++ stable_baselines/td3/td3.py
@@ -9,7 +9,6 @@ import tensorflow as tf
 from stable_baselines import logger
 from stable_baselines.common import tf_util, OffPolicyRLModel, SetVerbosity, TensorboardWriter
 from stable_baselines.common.vec_env import VecEnv
-from stable_baselines.common.tf_util import total_episode_reward_logger
 from stable_baselines.common.math_util import safe_mean, unscale_action, scale_action
 from stable_baselines.common.schedules import get_schedule_fn
 from stable_baselines.common.buffers import ReplayBuffer
